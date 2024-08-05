16817a9f2785b2e10f71dc5ac5406f1865e3adbb
JeffreyChix <chiedoziejeffrey@gmail.com>
2024-08-04 21:35:51 -0400

    report routes

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,6 +1,5 @@
 import dayjs from "dayjs";
 
-const getCurrentDate = () =>
-    dayjs().format("dddd, MMM DD, YYYY [at] h:mma"); // e.g., Sunday, Aug 18, 2024 at 12:48pm
+const getCurrentDate = () => dayjs().toISOString();
 
 export { getCurrentDate };
